4aba9dd39841e58304e7cad4b7dd950a5b3ff4cf
ISHARRR <ishar8@hotmail.com>
2021-02-08 16:00:23 +0000

    More Hackerrank - JAVA

---
+++ Java/src/com/company/MinimumAbsoluteDiffInArray.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class MinimumAbsoluteDiffInArray {
+    // Complete the minimumAbsoluteDifference function below.
+    static int minimumAbsoluteDifference(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int temp = 0;
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        for(int i = 0; i < arr.length-1; i++){
+            temp = Math.abs(arr[i] - arr[i+1]);
+            if (temp < min){
+                min = temp;
+            }
+        }
+
+        // for(int i = 0; i < arr.length; i++){
+        //     for(int j = 0; j < arr.length; j++){
+        //         if (i != j) {
+        //             temp = Math.abs(arr[i] - arr[j]);
+        //             if (temp < min){
+        //                 min = temp;
+        //             }
+        //         }
+        //     }
+        // }
+        return min;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = minimumAbsoluteDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+}
